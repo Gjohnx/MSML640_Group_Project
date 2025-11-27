264ebf71983770f73aff0ba2a9ae9c95d8e5b544
Gjohnx <gjohnx88@gmail.com>
2025-11-27 17:26:08 -0500

    Load JSON and Kociemba solver

--- core/kociemba_solver.py
+++ core/kociemba_solver.py
@@ -5,3 +5,15 @@ solve(cube_state):
 Implements the advanced Kociemba's Two-Phase Algorithm or integrates an external library for it. 
 Returns a short, optimal move sequence.
 '''
+import kociemba
+
+kociemba = None
+
+# This method utilized kociemba library to solve the cube with Kociemba method.
+def solve(cube_state_string: str) -> str:
+
+    if kociemba is None:
+        raise NotImplementedError("Kociemba library not installed. Run 'pip install kociemba'")
+    
+    # self.state format to the string format kociemba expects (URFDLB).
+    return kociemba.solve(cube_state_string)
