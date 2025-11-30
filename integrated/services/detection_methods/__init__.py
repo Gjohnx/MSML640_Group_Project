578701cd1a4a42614ba6fdcfc8ccd68bdf9180ae
dcalabresi-umd <dcalabre@umd.edu>
2025-11-30 14:36:19 -0500

    Minor fix

--- integrated/services/detection_methods/__init__.py
+++ integrated/services/detection_methods/__init__.py
@@ -1,7 +1,6 @@
 from .base import DetectionMethod
 from .void import VoidDetectionMethod
 from .random import RandomDetectionMethod
-from .blob import BlobDetectionMethod
 from .color_grid import ColorGridDetectionMethod
 
 __all__ = ['DetectionMethod', 'VoidDetectionMethod', 'RandomDetectionMethod', 'BlobDetectionMethod', 'ColorGridDetectionMethod']
