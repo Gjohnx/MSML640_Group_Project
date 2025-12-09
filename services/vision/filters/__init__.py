c13aa2ace9e340701cc53113d84b6b3b19f2ee13
JasonYan1030 <ryan0328@umd.edu>
2025-12-09 00:00:03 -0500

    Delete services directory

--- services/vision/filters/__init__.py
+++
@@ -1,6 +0,0 @@
-from .video_filter import VideoFilter
-from .remove_glare_filter import RemoveGlareFilter
-from .canny_edge_filter import CannyEdgeFilter
-
-__all__ = ['VideoFilter', 'RemoveGlareFilter', 'CannyEdgeFilter']
-
--- services/vision/filters/video_filter.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-import numpy as np
-
-class VideoFilter(ABC):
-    
-    @abstractmethod
-    def apply(self, img: np.ndarray) -> np.ndarray:
-        pass
-
