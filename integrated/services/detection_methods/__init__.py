46d96b2d75d5ae2973a2b1e528fe1f356ea35015
JasonYan1030 <ryan0328@umd.edu>
2025-12-08 23:59:09 -0500

    Delete integrated directory

--- integrated/services/detection_methods/__init__.py
+++
@@ -1,9 +0,0 @@
-from .base import DetectionMethod
-from .void import VoidDetectionMethod
-from .hardcoded import HardcodedDetectionMethod
-from .random import RandomDetectionMethod
-from .deepl.yolo import YOLODetectionMethod
-from .deepl.yolo_cnn import YOLOCNNDetectionMethod
-
-__all__ = ['DetectionMethod', 'VoidDetectionMethod', 'HardcodedDetectionMethod', 'RandomDetectionMethod', 'YOLODetectionMethod', 'YOLOCNNDetectionMethod']
-
